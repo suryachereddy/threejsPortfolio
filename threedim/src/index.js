04185571ccb0dd02f2d0b7f27c2d86241f3597cc
Surya Chereddy <45197683+suryachereddy@users.noreply.github.com>
2023-10-31 15:21:38 -0700

    Delete threedim directory

--- threedim/src/index.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import Container from "./App";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<Container />, rootElement);
